3413b2bc9aabc7fe046e3b76d2d8a9a485ecd009
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-07-03 12:30:22 -0700

    additions to JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 let specialEl = ['!', '@', '#', '$','%', '^','&','*', '(',')'];
-let lowerCase = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'u', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'U', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let number = ['1','2','3','4','5','6','7','8','9','0'];
+let alpha = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'u', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+space = [];
+
+//add numeric variables. 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -26,16 +29,24 @@ function passwordCriteria() {
   //var hadUpper =confirm() also do for the upper and lower 
   // var when i answer a prompt than my input should be calidated with one cha type sho
   var hasUpper = confirm('click to confirm');
+  var hadSpecial = confirm('click to confirm');
 };
 //Write the code to diplay the actual password, using a function, 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
   var options = passwordCriteria();
+  //if statements with the .concat
+
+  let passwordFinal = []
+
 
 // create an empty arry of charters to be seen 
 }
 
+for () {
+  let generatePassword i =1; i < 10; i++;
+};
 //random index 
 //build a loop to create the pass word for loop for(var i=), i< varable i++
 //use while to keep the going 
